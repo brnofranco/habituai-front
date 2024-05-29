659b5cee3efa970e5d8640b79ad463c54905ff99
Bruno <brunofrancodemoraes@gmail.com>
2024-05-28 22:02:16 -0300

    remove user account env and add register env

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 export const envs = {
     baseURL: import.meta.env.VITE_HOST_BACKEND || 'http://localhost:8080',
     loginPath: import.meta.env.VITE_LOGIN_PATH || '/api/v1/user/login',
+    registerPath: import.meta.env.VITE_REGISTER_PATH || '/api/v1/user/register',
     userPath: import.meta.env.VITE_USER_PATH || '/api/v1/user',
     habitPath: import.meta.env.VITE_HABIT_PATH || '/api/v1/habit',
     habitWeekPath: import.meta.env.VITE_HABIT_WEEK_PATH || '/api/v1/habit/week',
     progressPath: import.meta.env.VITE_PROGRESS_PATH || '/api/v1/progress',
-    userAccountPath: import.meta.env.VITE_USER_ACCOUNT_PATH || '/api/v1/user/account',
     reportPath: import.meta.env.VITE_REPORT_PATH || '/api/v1/report',
     achievementPath: import.meta.env.VITE_ACHIEVEMENT_PATH || '/api/v1/userAchievement/achievements',
 };
